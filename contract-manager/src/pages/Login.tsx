b91f7cc15237cab36472c5b4352bba4deda41a35
GeorgFehringer <92082503+GeorgFehringer@users.noreply.github.com>
2024-05-04 16:17:38 +0200

    chore: Add login page with CSS styling and form functionality

--- contract-manager/src/pages/Login.tsx
+++ contract-manager/src/pages/Login.tsx
@@ -1,5 +1,41 @@
+import React, { useState } from 'react';
+import "./Login.css";
+
 function Login() {
-  return <></>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // TODO: Implement login logic
+  };
+
+  return (
+    <div className="login-background">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+        </label>
+        <input type="text" placeholder="Username" value={username} onChange={handleUsernameChange} />
+        <br />
+        <label>
+          Password:
+        </label>
+        <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
